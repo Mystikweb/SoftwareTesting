edb2976a327d4347af6c72269d73485244cf9dde
Christopher Hair <mystikweb@live.ca>
2012-10-14 14:44:40 -0400

    Setup test file

---
+++ SoftwareTesting/test/lab3fall2012/Lab3Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3fall2012;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import mohawkcollege.comp10066.perflib.Sort;
+
+/**
+ *
+ * @author Christopher
+ */
+public class Lab3Test {
+    
+    public Lab3Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Lab3.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Lab3.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
