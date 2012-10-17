2d7607b1d6495a5b2bd06cfe645f5df382f61859
Christopher Hair <mystikweb@live.ca>
2012-10-17 13:39:18 -0400

    Basic Test Written

--- SoftwareTesting/test/lab3fall2012/Lab3Test.java
+++ SoftwareTesting/test/lab3fall2012/Lab3Test.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import mohawkcollege.comp10066.perflib.Sort;
+import org.junit.Assert;
 
 /**
  *
@@ -39,14 +40,232 @@ public class Lab3Test {
     }
 
     /**
-     * Test of main method, of class Lab3.
+     * 
      */
     @Test
-    public void testMain() {
-        System.out.println("main");
-        String[] args = null;
-        Lab3.main(args);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSortADefault()
+    {
+        int size = 5;
+        long started, total;
+        int numbers[] = new int[size];
+        int expected[] = new int[size];
+        
+        expected[0] = 1;
+        expected[1] = 2;
+        expected[2] = 3;
+        expected[3] = 4;
+        expected[4] = 5;
+                
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 1;
+        
+        System.out.print("Before Sort A: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        
+        started = System.nanoTime();
+        Sort.a(numbers);
+        total = (System.nanoTime() - started) / 1000;
+                
+        System.out.print("After Sort A: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        System.out.println("Total Time to Run Sort A: " + total + "us");
+        System.out.println();
+        
+        Assert.assertArrayEquals(expected, numbers);
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testSortBDefault()
+    {
+        int size = 5;
+        long started, total;
+        int numbers[] = new int[size];
+        int expected[] = new int[size];
+        
+        expected[0] = 1;
+        expected[1] = 2;
+        expected[2] = 3;
+        expected[3] = 4;
+        expected[4] = 5;
+                
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 1;
+        
+        System.out.print("Before Sort B: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        
+        started = System.nanoTime();
+        Sort.b(numbers);
+        total = (System.nanoTime() - started) / 1000;
+                
+        System.out.print("After Sort B: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        System.out.println("Total Time to Run Sort B: " + total + "us");
+        System.out.println();
+        
+        Assert.assertArrayEquals(expected, numbers);
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testSortCDefault()
+    {
+        int size = 5;
+        long started, total;
+        int numbers[] = new int[size];
+        int expected[] = new int[size];
+        
+        expected[0] = 1;
+        expected[1] = 2;
+        expected[2] = 3;
+        expected[3] = 4;
+        expected[4] = 5;
+                
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 1;
+        
+        System.out.print("Before Sort C: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        
+        started = System.nanoTime();
+        Sort.c(numbers);
+        total = (System.nanoTime() - started) / 1000;
+                
+        System.out.print("After Sort C: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        System.out.println("Total Time to Run Sort C: " + total + "us");
+        System.out.println();
+        
+        Assert.assertArrayEquals(expected, numbers);
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testSortDDefault()
+    {
+        int size = 5;
+        long started, total;
+        int numbers[] = new int[size];
+        int expected[] = new int[size];
+        
+        expected[0] = 1;
+        expected[1] = 2;
+        expected[2] = 3;
+        expected[3] = 4;
+        expected[4] = 5;
+                
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 1;
+        
+        System.out.print("Before Sort D: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        
+        started = System.nanoTime();
+        Sort.d(numbers);
+        total = (System.nanoTime() - started) / 1000;
+                
+        System.out.print("After Sort D: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        System.out.println("Total Time to Run Sort D: " + total + "us");
+        System.out.println();
+        
+        Assert.assertArrayEquals(expected, numbers);
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testSortEDefault()
+    {
+        int size = 5;
+        long started, total;
+        int numbers[] = new int[size];
+        int expected[] = new int[size];
+        
+        expected[0] = 1;
+        expected[1] = 2;
+        expected[2] = 3;
+        expected[3] = 4;
+        expected[4] = 5;
+                
+        numbers[0] = 5;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 1;
+        
+        System.out.print("Before Sort E: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        
+        started = System.nanoTime();
+        Sort.e(numbers);
+        total = (System.nanoTime() - started) / 1000;
+                
+        System.out.print("After Sort E: ");
+        for(int i = 0; i < numbers.length; i++)
+        {
+            System.out.printf("%d, ", numbers[i]);
+        }
+        System.out.println();
+        System.out.println("Total Time to Run Sort E: " + total + "us");
+        System.out.println();
+        
+        Assert.assertArrayEquals(expected, numbers);
     }
 }
