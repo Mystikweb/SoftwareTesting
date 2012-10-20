11ff143b8f2e652725754e50d6a71ea57afc9e70
Christopher Hair <mystikweb@live.ca>
2012-10-20 12:05:33 -0400

    Completed Program in Lab3 to test up to 100000 elements and put in tests for 1000000 elements in the Unit Test file.
    Just need to get everything commented now

--- SoftwareTesting/src/lab3fall2012/Lab3.java
+++ SoftwareTesting/src/lab3fall2012/Lab3.java
@@ -14,49 +14,179 @@ import mohawkcollege.comp10066.perflib.Sort;
  * @author mark.yendt
  */
 public class Lab3 {
-  /**
-   * creates an integer array of random integers using a student id as a seed
-   *
-   * @param size - number of elements must be a positive value greater than 1
-   * @param studid - a Mohawk college student id (no leading zeros)
-   * @return an array of the specified length
-   */
-  private static int[] buildrandomarray(int size, int studentId) {
+    
+    private static int studentId;
+    private static final int MINSIZE = 10;
+    private static final int MAXSIZE = 1000000;
+    
+    /**
+    * creates an integer array of random integers using a student id as a seed
+    *
+    * @param size - number of elements must be a positive value greater than 1
+    * @param studid - a Mohawk college student id (no leading zeros)
+    * @return an array of the specified length
+    */
+    private static int[] buildrandomarray(int size, int studentId) {
 
-    Random r = new Random(studentId);  // Student Id is used to ensure same set for comparison purposes
-    int array[] = new int[size];
+        Random r = new Random(studentId);  // Student Id is used to ensure same set for comparison purposes
+        int array[] = new int[size];
 
-    for (int i = 0; i < array.length; i++) {
-      array[i] = r.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt();
+        }
+        return array;
     }
-    return array;
-  }
   
-  /**
-   * Example of a manual timing test
-   *
-   * @param args
-   */
-  public static void main(String[] args) {
-    // YOU MUST ENTER YOUR STUDENT NUMBER HERE
-    final int studentId = 44556345;
-    final int size = 10000;
-    long      start, t;
+    /**
+    * Runs the Tests and provides output in summary format
+    *
+    * @param args
+    */
+    public static void main(String[] args) {
+        System.out.printf("Setting up Environment...\n");
+        
+        studentId = 000243034;
+        int size = MINSIZE;
+        long t;
 
-    // Declare random int array
-    int array[] = buildrandomarray(size, studentId);
+        CompileHelper();
+        
+        while(size < MAXSIZE) {
+            System.out.printf("\nElements To Sort: %d\n", size);
+            System.out.printf("-------------------------\n");
+            System.out.printf("Method\t\tTime\n");
+            System.out.printf("------\t\t----\n");
 
-    // Note call each sort method once to force compile (The java VM is a hotspot compiler)
-    Sort.a(array); 
+            t = SortATime(size);
+            System.out.printf("Sort A\t\t%d us\n", t);
 
-    // Timing example code 
-    int n = size;
-    start = System.nanoTime();
-    // Needs to be done before each sort call (fairness)
-    array = buildrandomarray(n, studentId);
-    Sort.a(array);
-    // Time is reported in microseconds (1 millionth of a second)
-    t = (System.nanoTime() - start) / 1000;
-    System.out.printf("Sort A took %d us to sort %d elements\n", t, n);
-  }
+            t = SortBTime(size);
+            System.out.printf("Sort B\t\t%d us\n", t);
+
+            t = SortCTime(size);
+            System.out.printf("Sort C\t\t%d us\n", t);
+
+            t = SortDTime(size);
+            System.out.printf("Sort D\t\t%d us\n", t);
+
+            t = SortETime(size);
+            System.out.printf("Sort E\t\t%d us\n", t);
+            
+            size *= 10;
+        }
+        
+        System.out.printf("\nTo see results for the 1000000 Element testing please run the associated JUnit Test file\n\n");
+    }
+  
+    /**
+     * 
+     */
+    private static void CompileHelper()
+    {
+        int size = 5;
+        
+        int array[] = buildrandomarray(size, studentId);
+        Sort.a(array);
+        
+        array  = buildrandomarray(size, studentId);
+        Sort.b(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.c(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.d(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.e(array);
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return 
+     */
+    private static long SortATime(int size)
+    {
+        long start;
+        
+        // Create an array to be sorted
+        int array[] = buildrandomarray(size, studentId);
+        Sort.a(array); 
+
+        // Time the sort after building a new array 
+        array = buildrandomarray(size, studentId);
+        start = System.nanoTime();
+        Sort.a(array);
+        
+        return (System.nanoTime() - start) / 1000;
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return 
+     */
+    private static long SortBTime(int size)
+    {
+        long start;
+
+        // Time the sort after building a new array
+        int array[] = buildrandomarray(size, studentId);
+        start = System.nanoTime();
+        Sort.b(array);
+        
+        return (System.nanoTime() - start) / 1000;
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return 
+     */
+    private static long SortCTime(int size)
+    {
+        long start;
+
+        // Time the sort after building a new array
+        int array[] = buildrandomarray(size, studentId);
+        start = System.nanoTime();
+        Sort.c(array);
+        
+        return (System.nanoTime() - start) / 1000;
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return 
+     */
+    private static long SortDTime(int size)
+    {
+        long start;
+
+        // Time the sort after building a new array
+        int array[] = buildrandomarray(size, studentId);
+        start = System.nanoTime();
+        Sort.d(array);
+        
+        return (System.nanoTime() - start) / 1000;
+    }
+    
+    /**
+     * 
+     * @param size
+     * @return 
+     */
+    private static long SortETime(int size)
+    {
+        long start;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(size, studentId);
+        start = System.nanoTime();
+        Sort.e(array);
+        
+        return (System.nanoTime() - start) / 1000;
+    }
 }
--- SoftwareTesting/test/lab3fall2012/Lab3Test.java
+++ SoftwareTesting/test/lab3fall2012/Lab3Test.java
@@ -1,9 +1,27 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * <p><strong>Statement of Authorship</strong><br />
+ * <strong style="color: red">
+ * I Christopher Hair, 000243034 certify that this material is my 
+ * original work. No other person's work has been used without suitable
+ * acknowledgment and I have not made my work available to anyone else.
+ * </strong></p>
+ * 
+ * <pre>
+ * File: ArraySetUtilitiesTest.java
+ * Date: 10/05/12
+ * Author: Christopher Hair
+ * Student ID: 000243034
+ * </pre>
+ *
+ * <h2 style="color: blue">Description:</h2>
+ * <p>Unit tests to test the provided Sort class</p>
+ * 
+ * @author Christopher Hair
+ * @author Student ID 000243034 
  */
 package lab3fall2012;
 
+import java.util.Random;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -15,12 +33,17 @@ import mohawkcollege.comp10066.perflib.Sort;
 import org.junit.Assert;
 
 /**
- *
+ * Tests for sort methods in the Sort class provided
+ * 
  * @author Christopher
  */
 public class Lab3Test {
     
+    private static int studentId; // Used for seeding the random generators
+    private static final int MAXSIZE = 1000000; // Dataset size to be sorted
+    
     public Lab3Test() {
+        studentId = 000243034;
     }
     
     @BeforeClass
@@ -31,8 +54,27 @@ public class Lab3Test {
     public static void tearDownClass() {
     }
     
+    /**
+     * Used to let the jvm do an initial compile of the methods before tests
+     */
     @Before
     public void setUp() {
+        int size = 5;
+        
+        int array[] = buildrandomarray(size, studentId);
+        Sort.a(array);
+        
+        array  = buildrandomarray(size, studentId);
+        Sort.b(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.c(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.d(array); 
+        
+        array = buildrandomarray(size, studentId);
+        Sort.e(array); 
     }
     
     @After
@@ -40,11 +82,30 @@ public class Lab3Test {
     }
 
     /**
+    * creates an integer array of random integers using a student id as a seed
+    *
+    * @param size - number of elements must be a positive value greater than 1
+    * @param studid - a Mohawk college student id (no leading zeros)
+    * @return an array of the specified length
+    */
+    private static int[] buildrandomarray(int size, int studentId) {
+
+        Random r = new Random(studentId);  // Student Id is used to ensure same set for comparison purposes
+        int array[] = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = r.nextInt();
+        }
+        return array;
+    }
+    
+    /**
      * 
      */
     @Test
     public void testSortADefault()
     {
+        System.out.println("Default Test for Sort A");
         int size = 5;
         long started, total;
         int numbers[] = new int[size];
@@ -91,6 +152,7 @@ public class Lab3Test {
     @Test
     public void testSortBDefault()
     {
+        System.out.println("Default Test for Sort B");
         int size = 5;
         long started, total;
         int numbers[] = new int[size];
@@ -137,6 +199,7 @@ public class Lab3Test {
     @Test
     public void testSortCDefault()
     {
+        System.out.println("Default Test for Sort C");
         int size = 5;
         long started, total;
         int numbers[] = new int[size];
@@ -183,6 +246,7 @@ public class Lab3Test {
     @Test
     public void testSortDDefault()
     {
+        System.out.println("Default Test for Sort D");
         int size = 5;
         long started, total;
         int numbers[] = new int[size];
@@ -229,6 +293,7 @@ public class Lab3Test {
     @Test
     public void testSortEDefault()
     {
+        System.out.println("Default Test for Sort E");
         int size = 5;
         long started, total;
         int numbers[] = new int[size];
@@ -267,5 +332,87 @@ public class Lab3Test {
         System.out.println();
         
         Assert.assertArrayEquals(expected, numbers);
+    }
+    
+    /* LARGE DATASET TESTING */
+    
+    @Test(timeout=30000)
+    public void testSortALargeVol() 
+    {
+        System.out.println("Large DataSet Test for Sort A(Stops in 30s if no results)");
+        long start, time;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(MAXSIZE, studentId);
+        start = System.nanoTime();
+        Sort.a(array);
+        
+        time = (System.nanoTime() - start) / 1000;
+        
+        System.out.printf("Sort A took %d us to sort %d elements\n", time, MAXSIZE);
+    }
+    
+    @Test(timeout=30000)
+    public void testSortBLargeVol() 
+    {
+        System.out.println("Large DataSet Test for Sort B(Stops in 30s if no results)");
+        long start, time;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(MAXSIZE, studentId);
+        start = System.nanoTime();
+        Sort.b(array);
+        
+        time = (System.nanoTime() - start) / 1000;
+        
+        System.out.printf("Sort B took %d us to sort %d elements\n", time, MAXSIZE);
+    }
+    
+    @Test(timeout=30000)
+    public void testSortCLargeVol() 
+    {
+        System.out.println("Large DataSet Test for Sort C(Stops in 30s if no results)");
+        long start, time;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(MAXSIZE, studentId);
+        start = System.nanoTime();
+        Sort.c(array);
+        
+        time = (System.nanoTime() - start) / 1000;
+        
+        System.out.printf("Sort C took %d us to sort %d elements\n", time, MAXSIZE);
+    }
+    
+    @Test(timeout=30000)
+    public void testSortDLargeVol() 
+    {
+        System.out.println("Large DataSet Test for Sort D(Stops in 30s if no results)");
+        long start, time;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(MAXSIZE, studentId);
+        start = System.nanoTime();
+        Sort.d(array);
+        
+        time = (System.nanoTime() - start) / 1000;
+        
+        System.out.printf("Sort D took %d us to sort %d elements\n", time, MAXSIZE);
+    }
+    
+    @Test(timeout=30000)
+    public void testSortELargeVol() 
+    {
+        System.out.println("Large DataSet Test for Sort E(Stops in 30s if no results)");
+        long start, time;
+
+        // Time the sort after building a new array 
+        int array[] = buildrandomarray(MAXSIZE, studentId);
+        start = System.nanoTime();
+        Sort.e(array);
+        
+        time = (System.nanoTime() - start) / 1000;
+        
+        System.out.printf("Sort E took %d us to sort %d elements\n", time, MAXSIZE);
     }
 }
